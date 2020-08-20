ee778264fd5ef451b2c97158089689fd6f9b3501
Myoji nicname <abc@example.com>
2020-08-20 16:18:47 +0900

    add function

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func division() {
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func multiplication() {
+        number = number * 2
+        label.text = String(number)
+    }
+    
 }
 
